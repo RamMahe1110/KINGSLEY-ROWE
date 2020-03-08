0e17d84f7ddae841cb7d97025c726cedc5b97ebb
Ram Maheshwari <ram991011@gmail.com>
2020-03-08 23:18:49 +0530

    Updates

---
+++ precache-manifest.6e305a2bd827ad2e21d23ec887a2ff1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668722b19dcde02ef18f6c65b30645e7",
+    "url": "/KINGSLEY-ROWE-FRONTEND-WEB/index.html"
+  },
+  {
+    "revision": "c3c658401b121fd75cd0",
+    "url": "/KINGSLEY-ROWE-FRONTEND-WEB/static/css/main.44524516.chunk.css"
+  },
+  {
+    "revision": "7cfbf4c7bb8251ebf128",
+    "url": "/KINGSLEY-ROWE-FRONTEND-WEB/static/js/2.1d3b57f4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/KINGSLEY-ROWE-FRONTEND-WEB/static/js/2.1d3b57f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c658401b121fd75cd0",
+    "url": "/KINGSLEY-ROWE-FRONTEND-WEB/static/js/main.a1f684e2.chunk.js"
+  },
+  {
+    "revision": "4bf0d567d52bb1ea7c1b",
+    "url": "/KINGSLEY-ROWE-FRONTEND-WEB/static/js/runtime-main.533d804b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b05d9f43ce84ec3850cde5c7306da921.js"
+  "/KINGSLEY-ROWE-FRONTEND-WEB/precache-manifest.6e305a2bd827ad2e21d23ec887a2ff1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KINGSLEY-ROWE-FRONTEND-WEB/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

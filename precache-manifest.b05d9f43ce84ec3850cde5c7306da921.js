faf541fb30637f18450b95cba2296d8eb372b72c
Ram Maheshwari <ram991011@gmail.com>
2020-03-08 23:11:11 +0530

    Updates

---
+++ precache-manifest.b05d9f43ce84ec3850cde5c7306da921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e7a098f71c457ff5b4e8e149b0bcbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d331aceb9097a022e99",
+    "url": "/static/css/main.44524516.chunk.css"
+  },
+  {
+    "revision": "7cfbf4c7bb8251ebf128",
+    "url": "/static/js/2.1d3b57f4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1d3b57f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d331aceb9097a022e99",
+    "url": "/static/js/main.42a20165.chunk.js"
+  },
+  {
+    "revision": "b60b4d1e16ac0fdc2a81",
+    "url": "/static/js/runtime-main.eebd62b5.js"
+  }
+]);
